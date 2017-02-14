f6f22c8789467e634f7ff30926d78b0e938241f2
NALS-ANHDV <anhdv@nal.vn>
2017-02-14 09:27:38 +0700

    animate bus

--- app/src/main/java/com/anhdv/buslocal/MapsActivity.java
+++ app/src/main/java/com/anhdv/buslocal/MapsActivity.java
@@ -2,20 +2,28 @@ package com.anhdv.buslocal;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 import android.Manifest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONException;
@@ -29,6 +37,8 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
         ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
+    private static final String TAG = "BUS DEMO";
+    Marker now;
 
     /**
      * Request code for location permission request.
@@ -48,47 +58,55 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        BusApplication app = (BusApplication) getApplication();
-        mSocket = app.getSocket();
-        //mSocket.on(Socket.EVENT_CONNECT, onConnect);
-        mSocket.on("moving", onMoving);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        BusApplication app = (BusApplication) getApplication();
+        mSocket = app.getSocket();
+        mSocket.on(Socket.EVENT_CONNECT,onConnect);
         mSocket.connect();
-        mSocket.emit("start","");
+
+
     }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+        mSocket.off("moving", onMoving);
+
+    }
+
     private Emitter.Listener onConnect = new Emitter.Listener() {
         @Override
         public void call(Object... args) {
-            JSONObject data = (JSONObject) args[0];
+            //JSONObject data = (JSONObject) args[0];
 
-            Log.d("loc:",data.toString());
+           // Log.d("loc:",data.toString());
+            mSocket.emit("start","");
         }
     };
     private Emitter.Listener onMoving = new Emitter.Listener() {
         @Override
-        public void call(Object... args) {
-            JSONObject data = (JSONObject) args[0];
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    Log.d("ZungX", "data On Moving: " + data.toString());
+                    try {
+                        Double lon = Double.parseDouble(data.getString("longitude"));
+                        Double lat = Double.parseDouble(data.getString("latitude"));
+                        LatLng loc =  new LatLng(lat,lon);
+                        //drawCustomMarker(loc);
+                        animateMarker(now,loc,true);
 
-            String[] latLng = data.optString("longlat").split(",");
-            //LatLng loc = (LatLng) data.optString("longlat");
-
-            String numUsers;
-            try {
-                numUsers = data.getString("longlat");
-                Log.d("loc:",numUsers);
-            } catch (JSONException e) {
-                return;
-            }
-
-//            Intent intent = new Intent();
-//            intent.putExtra("username", mUsername);
-//            intent.putExtra("numUsers", numUsers);
-//            setResult(RESULT_OK, intent);
-            finish();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
     };
     /**
@@ -100,19 +118,118 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    private void drawCustomMarker(LatLng point) {
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(point);
-        markerOptions.title("SMS");
-        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_cast_dark));
-        mMap.addMarker(markerOptions);
+    public void animateMarker(final Marker marker, final LatLng toPosition,
+                              final boolean hideMarker) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = mMap.getProjection();
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 1000;
+        final Interpolator interpolator = new LinearInterpolator();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed
+                        / duration);
+                double lng = t * toPosition.longitude + (1 - t)
+                        * startLatLng.longitude;
+                double lat = t * toPosition.latitude + (1 - t)
+                        * startLatLng.latitude;
+                marker.setPosition(new LatLng(lat, lng));
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+                    if (hideMarker) {
+                        marker.setVisible(true);
+                    } else {
+                        marker.setVisible(true);
+                    }
+                }
+            }
+        });
+    }
+    private void drawCustomMarker(LatLng latLng) {
+//        if(now != null){
+//            now.remove();
+//
+//        }
+
+        now = mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("San Francisco")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_directions_bus))
+                .draggable(true)
+                .snippet("Population: 776733"));
+        // Showing the current location in Google Map
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+       // mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(point);
+//        markerOptions.title("SMS");
+        //markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_audiotrack));
+       // mMap.addMarker(markerOptions);
+//        Marker marker = mMap.addMarker(new MarkerOptions()
+//                .position(point)
+//                .title("San Francisco")
+//                .snippet("Population: 776733"));
+//        MarkerOptions markerOptions = new MarkerOptions().position(point).title(
+//                "Current Location").draggable(true);
+//        mMap.addMarker(markerOptions);
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        mMap.setOnMyLocationButtonClickListener(this);
+        //LatLng sydney = new LatLng(16.087413, 108.215262);
+        //drawCustomMarker(sydney);
+        //mMap.setOnMyLocationButtonClickListener(this);
         enableMyLocation();
+
+        LatLng nal = new LatLng(16.0872849, 108.2155529);
+        drawCustomMarker(nal);
+//        LatLng nal = new LatLng(-35.281945,149.130034);
+
+        // create marker
+//        MarkerOptions marker = new MarkerOptions().position(nal).title("Test user").snippet("Hellooo");
+//        marker.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker));
+//        mMap.addMarker(marker);
+//
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(nal).zoom(16).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                Log.d(TAG, "latitude : "+ marker.getPosition().latitude);
+                marker.setSnippet(String.valueOf(marker.getPosition().latitude));
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+            }
+
+        });
+//
+//        MarkerOptions markerBusOpt = new MarkerOptions().title("Bus").position(nal).snippet("Bus no.1");
+//        markerBusOpt.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_directions_bus));
+//        markerBusDemo = mMap.addMarker(markerBusOpt);
+//        markerBusDemo.setVisible(false);
+
+
+        mSocket.on("moving", onMoving);
+        mSocket.connect();
+
+
     }
 
     /**
@@ -134,7 +251,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
     public boolean onMyLocationButtonClick() {
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
         //socket.emit(‘start’, {});
-        //mSocket.emit("start","");
+
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
